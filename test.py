170c7dcc89f2b33a07037fce0a81d969538b42c2
soncnjp181006 <tranxuansoncnjp181006@gmail.com>
2026-03-06 10:49:05 +0700

    Hướng dẫn kiểu dữ liệu trong SQLALCHEMY

--- test.py
+++ test.py
@@ -22,50 +22,16 @@ engine = create_engine(
 #############################################################################################
 # Thực hiện tạo bảng, ... học truy vấn ở đây, phần bên trên là kết nối database
 #############################################################################################
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float, Numeric, Boolean, Text
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
 Base = declarative_base()
 
-class Departments(Base):
-   __tablename__ = 'departments_sqlalchemy'
-   dept_id=Column(Integer, autoincrement=True, primary_key=True)
-   dept_name=Column(String(100), nullable=False)
-   location=Column(String(255))
+class User(Base):
+    # Tên bảng trong MySQL
+    __tablename__ = 'users'
 
-class Employees(Base):
-   __tablename__ = 'employees_sqlalchemy'
-   emp_id=Column(Integer, autoincrement=True, primary_key=True)
-   emp_name=Column(String(100), nullable=False)
-   dept_id=Column(
-      Integer,
-      ForeignKey(
-         'departments_sqlalchemy.dept_id',
-         ondelete='RESTRICT',
-         onupdate='CASCADE'
-      ),
-      nullable=False
-   )
-
-# Tạo bảng khi bảng chưa tồn tại
-Base.metadata.create_all(bind=engine)
-print("Tạo bảng thành công")
-
-
-# Chèn dữ liệu bằng Session
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# Thêm dữ liệu vào Departments
-dept_1 = Departments(dept_name='IT', location='Ha Noi')
-dept_2 = Departments(dept_name='HR', location='Ho Chi Minh')
-
-session.add_all([dept_1, dept_2])
-
-session.commit() # Commit -> thực hiện lưu
-print('Chèn dữ liệu vào bảng departments_sqlalchemy thành công')
-
-departments = session.query(Departments).all()
-for dept in departments:
-   print(dept.dept_id, dept.dept_name, dept.location)
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    username=Column(String(200), nullable=False, unique=True)
+    
